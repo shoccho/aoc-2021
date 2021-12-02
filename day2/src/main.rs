8a6b550e72915bdc9aa47bc48c29bc77a2530e45
shoccho <shoccho@dcl.localhost>
2021-12-02 19:11:36 +0600

    day 2 better approach I guess?

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,5 +1,53 @@
+#[derive(Debug)]
+enum Command{
+    Forward(i64),
+    Up(i64),
+    Down(i64),
+}
+
+impl Command{
+    fn parse(s:&str)->Self{
+        let words :Vec<&str> = s.trim().split_whitespace().collect();
+        match words.as_slice(){
+            ["forward",val]=> (Command::Forward(val.parse().unwrap())),
+            ["up",val]=> (Command::Up(val.parse().unwrap())),
+            ["down",val]=> (Command::Down(val.parse().unwrap())),
+            _ =>panic!("This shouldn't happe"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Submarine {
+    x: i64,
+    y: i64
+}
+impl Submarine{
+    fn new()-> Submarine{
+        Submarine{
+            x:0,
+            y:0
+        }
+    }
+    fn operate(&mut self,command :Command){
+        match command{
+            Command::Up(value)=>{self.y-=value},
+            Command::Down(value)=>{self.y+=value},
+            Command::Forward(value)=>{self.x+=value},
+        }
+    }
+}
+
+
 fn main() {
     let lines = include_str!("input").lines().collect::<Vec<&str>>();
+    let mut submarine = Submarine::new();
+
+    let commands = include_str!("input").lines().map(Command::parse).collect::<Vec<Command>>();
+    for command in commands{
+        submarine.operate(command)
+    }
+    println!("{:?}",submarine.x * submarine.y );
 
     solve1(&lines);
     solve2(&lines);
@@ -15,7 +63,7 @@ fn solve2(lines:&[&str]){
         let val = tokens.next().unwrap().parse::<i32>().unwrap();
         if command =="forward"{
             x+=val;
-            y+= (aim * val);
+            y+= aim * val;
         }else if command =="up"{
             aim-=val;
         }else if command =="down"{
