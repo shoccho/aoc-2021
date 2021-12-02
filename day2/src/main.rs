46bd16400c678a4865f4af96962da03bbfd90970
shoccho <sfb.shoccho@gmail.com>
2021-12-02 19:51:40 +0600

    part 2 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,18 +1,18 @@
 #[derive(Debug)]
-enum Command{
+enum Command {
     Forward(i64),
     Up(i64),
     Down(i64),
 }
 
-impl Command{
-    fn parse(s:&str)->Self{
-        let words :Vec<&str> = s.trim().split_whitespace().collect();
-        match words.as_slice(){
-            ["forward",val]=> (Command::Forward(val.parse().unwrap())),
-            ["up",val]=> (Command::Up(val.parse().unwrap())),
-            ["down",val]=> (Command::Down(val.parse().unwrap())),
-            _ =>panic!("This shouldn't happe"),
+impl Command {
+    fn parse(s: &str) -> Self {
+        let words: Vec<&str> = s.trim().split_whitespace().collect();
+        match words.as_slice() {
+            ["forward", val] => (Command::Forward(val.parse().unwrap())),
+            ["up", val] => (Command::Up(val.parse().unwrap())),
+            ["down", val] => (Command::Down(val.parse().unwrap())),
+            _ => panic!("This shouldn't happe"),
         }
     }
 }
@@ -20,72 +20,44 @@ impl Command{
 #[derive(Debug)]
 struct Submarine {
     x: i64,
-    y: i64
+    y: i64,
+    aim: i64,
 }
-impl Submarine{
-    fn new()-> Submarine{
-        Submarine{
-            x:0,
-            y:0
+impl Submarine {
+    fn new() -> Submarine {
+        Submarine { x: 0, y: 0, aim: 0 }
+    }
+    fn operate(&mut self, command: &Command) {
+        match command {
+            Command::Up(value) => self.y -= value,
+            Command::Down(value) => self.y += value,
+            Command::Forward(value) => self.x += value,
         }
     }
-    fn operate(&mut self,command :Command){
-        match command{
-            Command::Up(value)=>{self.y-=value},
-            Command::Down(value)=>{self.y+=value},
-            Command::Forward(value)=>{self.x+=value},
+    fn operate_with_direction(&mut self, command: &Command) {
+        match command {
+            Command::Up(value) => self.aim -= value,
+            Command::Down(value) => self.aim += value,
+            Command::Forward(value) => {
+                self.x += value;
+                self.y += self.aim * value;
+            }
         }
     }
 }
-
 
 fn main() {
-    let lines = include_str!("input").lines().collect::<Vec<&str>>();
     let mut submarine = Submarine::new();
+    let mut submarine2 = Submarine::new();
 
-    let commands = include_str!("input").lines().map(Command::parse).collect::<Vec<Command>>();
-    for command in commands{
-        submarine.operate(command)
+    let commands = include_str!("input")
+        .lines()
+        .map(Command::parse)
+        .collect::<Vec<Command>>();
+    for command in commands {
+        submarine.operate(&command);
+        submarine2.operate_with_direction(&command);
     }
-    println!("{:?}",submarine.x * submarine.y );
-
-    solve1(&lines);
-    solve2(&lines);
-}
-
-fn solve2(lines:&[&str]){
-    let mut x :i32=0;
-    let mut y :i32=0;
-    let mut aim :i32=0;
-    for line in lines{
-        let mut tokens = line.split(' ');
-        let command =tokens.next().unwrap();
-        let val = tokens.next().unwrap().parse::<i32>().unwrap();
-        if command =="forward"{
-            x+=val;
-            y+= aim * val;
-        }else if command =="up"{
-            aim-=val;
-        }else if command =="down"{
-            aim+=val;
-        }
-    }
-    println!("{}",x*y);
-}
-fn solve1(lines:&[&str]){
-    let mut x :i32=0;
-    let mut y :i32=0;
-    for line in lines{
-        let mut tokens = line.split(' ');
-        let command =tokens.next().unwrap();
-        let val = tokens.next().unwrap().parse::<i32>().unwrap();
-        if command =="forward"{
-            x+=val;
-        }else if command =="up"{
-            y-=val;
-        }else if command =="down"{
-            y+=val;
-        }
-    }
-    println!("{}",x*y);
+    println!("Puzzle 1: {}", submarine.x * submarine.y);
+    println!("Puzzle 2: {}", submarine2.x * submarine2.y);
 }
